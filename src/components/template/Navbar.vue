948709ec15e8d2b8103abc61fb3f1138f8d40ee3
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2024-01-17 18:01:35 +0700

    Navbar !responsive

---
+++ src/components/template/Navbar.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="logo-navbar">
+        <div class="logo">
+            <div class="logo-1">
+                <a href=""><img src="@/assets/img/logowhite.png" alt=""></a>
+            </div>
+            <div class="logo-2">
+                <img src="@/assets/img/logoblack.png" alt="">
+            </div>
+        </div>
+        <div class="navbar">
+            <nav>
+                <a href=""><p>Home</p></a>
+                <a href=""><p>Menus</p></a>
+                <a href=""><p>About Us</p></a>
+                <a href=""><p>Gallery</p></a>
+                <a href=""><p>Contact</p></a>
+                <div class="hamburger">
+                    <i class="bi bi-list"></i>
+                </div>
+            </nav>
+        </div>
+        <div class="dark-light">
+            <div class="dark">
+                <i class="bi bi-moon-stars-fill"></i>
+            </div>
+            <div class="light">
+                <i class="bi bi-sun-fill"></i>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    .logo-navbar {
+        background-color: #1D1D1D;
+        display: flex;
+        justify-content: space-between;
+        padding: 10px 5%;
+    }
+
+    .logo-1 img {
+        width: 50px;
+        cursor: pointer;
+    }
+    
+    nav {
+        display: flex;
+        gap: 20px;
+        margin-bottom: -5px;
+    }
+    
+    nav a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    i {
+        font-size: 20px;
+        color: #fff;
+    }
+
+    .hamburger {
+        display: none;
+    }
+
+    .logo-2 img {
+        display: none;
+        width: 50px;
+    }
+
+    .dark {
+        margin-top: 10px;
+    }
+
+    .dark i {
+        color: #fff;
+        cursor: pointer;
+    }
+
+    .light {
+        display: none;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import './assets/main.css'
+import { createApp  } from 'vue';
+import App from './App.vue';
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+// bootstrap icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
+AOS.init();
 createApp(App).mount('#app')
